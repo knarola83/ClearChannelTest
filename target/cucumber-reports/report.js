5c1578bd17c0e9739d5384a6c4dbfe8a04ba369e
knarola83 <knarola83@gmail.com>
2019-04-05 21:40:50 +0100

    added redme file

--- target/cucumber-reports/report.js
+++ target/cucumber-reports/report.js
@@ -7,7 +7,101 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 8098265723,
+  "duration": 30482054705,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "To verify user should able to login successfully after entering correct credentials",
+  "description": "",
+  "id": "login;to-verify-user-should-able-to-login-successfully-after-entering-correct-credentials",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 4,
+      "name": "@login"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "user is on the login page of http://the-internet.herokuapp.com/login",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "the correct user credential \"tomsmith\"is entered in username field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "the correct password \"SuperSecretPassword!\" is entered in password field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "click on Login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "user is able to navigate on secure area of login page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepdefs.userIsOnTheLoginPageOfHttpTheInternetHerokuappComLogin()"
+});
+formatter.result({
+  "duration": 1549534402,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "tomsmith",
+      "offset": 29
+    }
+  ],
+  "location": "MyStepdefs.theCorrectUserCredentialIsEnteredInUsernameField(String)"
+});
+formatter.result({
+  "duration": 706115812,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "SuperSecretPassword!",
+      "offset": 22
+    }
+  ],
+  "location": "MyStepdefs.theCorrectPasswordIsEnteredInPasswordField(String)"
+});
+formatter.result({
+  "duration": 206681988,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.clickOnLoginButton()"
+});
+formatter.result({
+  "duration": 1371757317,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userIsAbleToNavigateOnSecureAreaOfLoginPage()"
+});
+formatter.result({
+  "duration": 130844355,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 622041688,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 6872450926,
   "status": "passed"
 });
 formatter.scenario({
@@ -53,7 +147,7 @@ formatter.match({
   "location": "MyStepdefs.userIsOnTheLoginPageOfHttpTheInternetHerokuappComLogin()"
 });
 formatter.result({
-  "duration": 434370210,
+  "duration": 86808,
   "status": "passed"
 });
 formatter.match({
@@ -66,7 +160,7 @@ formatter.match({
   "location": "MyStepdefs.theInValidUserCredentialIsEnteredInUsernameField(String)"
 });
 formatter.result({
-  "duration": 105012794,
+  "duration": 222414748,
   "status": "passed"
 });
 formatter.match({
@@ -79,25 +173,25 @@ formatter.match({
   "location": "MyStepdefs.theInValidPasswordIsEnteredInPasswordField(String)"
 });
 formatter.result({
-  "duration": 140813583,
+  "duration": 215721131,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.clickOnLoginButton()"
 });
 formatter.result({
-  "duration": 737429528,
+  "duration": 614119075,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.userIsNotAbleToNavigateOnSecureAreaOfLoginPage()"
 });
 formatter.result({
-  "duration": 54451665,
+  "duration": 676576312,
   "status": "passed"
 });
 formatter.after({
-  "duration": 630661758,
+  "duration": 1306500418,
   "status": "passed"
 });
 });
